781db8aab4194f595a38c3cdc5b195726ba2d992
Nico Grande <ngrande@chadwickschool.org>
2016-10-10 09:54:36 -0700

    Triangle Maker

--- Square-Lab.py
+++ Square-Lab.py
@@ -1,6 +1,6 @@
 from Myro import *
 init ("sim")
-
+#square
 penDown()
 forward (1,1)
 turnBy (-90)
@@ -11,3 +11,12 @@ turnBy (-90)
 forward (1,1)
 penUp()
 stop()
+#triangle
+forward (1,3)
+penDown()
+foward (1,1)
+turnBy (120)
+forward (1,1)
+turnBy (60)
+forward (1,1)
+penUp()
