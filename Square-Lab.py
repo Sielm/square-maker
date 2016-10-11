89d8f90b3cb77b85746c65ef0d6e3e6e79ba1da2
Nico <ngrande@chadwickschool.org>
2016-10-10 22:20:51 -0700

    Bug Fixes

--- Square-Lab.py
+++ Square-Lab.py
@@ -2,34 +2,36 @@ from Myro import *
 init ("sim")
 
 #square
+
 penDown()
-forward (1,1)
+forward (1,2)
 turnBy (-90)
-forward (1,1)
+forward (1,2)
 turnBy (-90)
-forward (1,1)
+forward (1,2)
 turnBy (-90)
-forward (1,1)
+forward (1,2)
 penUp()
 stop()
 
 #triangle
-forward (1,3)
+forward (1,2)
 penDown()
-foward (1,1)
+forward (1,2)
 turnBy (120)
-forward (1,1)
-turnBy (60)
-forward (1,1)
+forward (1,2)
+turnBy (120)
+forward (1,2)
 penUp()
 
 # letter "N"
 forward (1,3)
 penDown()
 forward (1,2)
-turnBy (120)
+backward (1,2)
+turnBy (30)
 forward (1,2)
-turnBy (-120)
+turnBy (150)
 forward (1,2)
-penUp()
 stop()
+
